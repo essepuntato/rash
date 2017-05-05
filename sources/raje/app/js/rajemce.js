bbc86cc9b5325ae9684a771734c603a77342286f
Gianmarco Spinaci <gianmarcospinaci@Gianmarcos-MBP.station>
2017-05-05 18:02:50 +0200

    1st instace of rajemce

--- sources/raje/app/js/rajemce.js
+++ sources/raje/app/js/rajemce.js
@@ -2,6 +2,12 @@
  * 
  * Initilize TinyMCE editor with all required options
  */
+
+// TinyMCE editor global reference
+let Editor
+
+const ZERO_SPACE = '&#8203;'
+
 tinymce.init({
 
   // Select the element to wrap
@@ -11,18 +17,36 @@ tinymce.init({
   content_css: ['css/bootstrap.min.css', 'css/rash.css'],
 
   // Set plugins
-  plugins: "fullscreen link",
+  plugins: "fullscreen link codesample inline_code",
 
   // Remove menubar
   menubar: false,
 
   // Custom toolbar
-  toolbar: 'undo redo bold italic link superscript subscript',
+  toolbar: 'undo redo bold italic link codesample superscript subscript inline_code',
 
   // Setup full screen on init
   setup: function (editor) {
+
+    // Setup global variable
+    Editor = editor
+
     editor.on('init', function (e) {
       editor.execCommand('mceFullScreen');
+    });
+
+    // Intercept enter key press
+    editor.on('KeyDown', function (e) {
+
+      // Enter key press event
+      if (e.keyCode == 13) {
+
+        // inline code
+        if (editor.selection.getNode().nodeName == 'CODE') {
+          e.preventDefault()
+          //Rajemce.inline.code.exit()
+        }
+      }
     });
   },
 
@@ -46,3 +70,57 @@ $(document).ready(function () {
   //attach whole body inside a placeholder div
   $('body').html(`<div id="raje_root">${$('body').html()}</div>`)
 })
+
+/**
+ * Inline code plugin RAJE
+ */
+tinymce.PluginManager.add('inline_code', function (editor, url) {
+
+  // Add a button that opens a window
+  editor.addButton('inline_code', {
+    text: 'inline_code',
+    icon: false,
+    tooltip: 'Inline code',
+
+    // Button behaviour
+    onclick: function () {
+      Rajemce.inline.code.insert()
+    }
+  });
+});
+
+/**
+ * RajeMCE class
+ * It contains every custom functions to attach to plugins
+ */
+Rajemce = {
+
+  // Inline elements
+  inline: {
+
+    // Inline code functions
+    code: {
+
+      /**
+       * Insert inline code element
+       * It checks if the selection is collapsed, and add the right element
+       */
+      insert: function () {
+
+        // Displayed text of the code
+        let text = ZERO_SPACE
+
+        // Check if the selection is collapsed
+        if (!Editor.selection.isCollapsed())
+          text = Editor.selection.getContent()
+
+        // Insert the element with the right text
+        Editor.execCommand('mceInsertContent', false, `<code>${text}</code>`)
+      },
+
+      exit: function () {
+
+      }
+    }
+  }
+}
